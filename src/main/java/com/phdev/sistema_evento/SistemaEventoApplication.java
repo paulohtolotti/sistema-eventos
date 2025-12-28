195c10b8a39d75f0556d2bb5e11e08dfb43a9870
phLinux <paulohtolotti@alumni.usp.br>
2025-12-28 18:20:14 -0300

    first commit

---
+++ src/main/java/com/phdev/sistema_evento/SistemaEventoApplication.java
@@ -0,0 +1,13 @@
+package com.phdev.sistema_evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phdev/sistema_evento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phdev.sistema_evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
